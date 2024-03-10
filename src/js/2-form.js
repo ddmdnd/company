65466f8f620e7bf59ba4eb974ba04dc85e804476
ddmdnd <139027634+ddmdnd@users.noreply.github.com>
2024-03-10 07:13:58 -0400

    Initial commit

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const form = document.querySelector(".feedback-form");
+// const textarea = form.querySelector(".form-label-textarea")
+form.addEventListener("input", takeDataForm);
+
+function takeDataForm(event) {
+    event.preventDefault();
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+    const dataForm = {
+        email,
+        message,
+    };
+    const localStringify = JSON.stringify(dataForm);
+    localStorage.setItem("feedback-form-state", localStringify);
+}
+putDataForm();
+function putDataForm() {
+    const storedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (storedData) {
+            form.elements.email.value = storedData.email;
+            form.elements.message.value = storedData.message;   
+    }
+}
+form.addEventListener("submit", function (event) {
+    if(form.elements.email.value.trim() && form.elements.message.value.trim()){
+    event.preventDefault();
+    console.log("Дані форми:", {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    });
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+}
+else {
+    event.preventDefault();
+    alert("Not all fields of the form are filled in");
+}
+});
+
